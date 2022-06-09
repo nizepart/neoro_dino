6fa90134b3f90cbfaf3ce3ed7343ee878a13dd05
Fehty <alioleg8@gmail.com>
2022-06-09 07:16:34 +0300

    Add refactoring and custom input

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from dino import run_game_cycle
 from neuro import run_neuro_cycle
 
 if __name__ == '__main__':
-    run_game_cycle()
     run_neuro_cycle()
+    run_game_cycle()
--- neuro.py
+++ neuro.py
@@ -6,6 +6,3 @@ from dino import invoke_jump_event
 def run_neuro_cycle():
     invoke_jump_event()
     threading.Timer(0.1, run_neuro_cycle).start()
-
-
-run_neuro_cycle()
